cb2fa0acfc7bcb3c7a2841298527ae657b79bff4
chic-page-6502 <zacharytq@gmail.com>
2019-08-02 00:59:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     data.each do |holiday, supplies|
       holiday_string = holiday.to_s.sub('_', ' ')
-      pry
+      
       puts "  #{}: #{supplies.join(", ")}"
     end
   end
